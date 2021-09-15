c08373e8f6a2806764ee43e4934175d302d7e1f1
cobywinfield <coby.winfield@gmail.com>
2021-09-15 19:08:02 -0400

    syntax changes

--- umd_python_cas/umd_python_cas.py
+++ umd_python_cas/umd_python_cas.py
@@ -4,7 +4,7 @@ from xmltodict import parse
 
 class UMDCASClient(object):
     '''
-    A python UMD Central Authentication Services remote CAS instance.
+    A python client to access the University of Maryland Central Authentication Services (CAS) remote instance.
 
     >>> from umd_python_cas import UMDCASClient
     '''
@@ -18,7 +18,7 @@ class UMDCASClient(object):
         self.umd_cas_ticket_url = 'https://shib.idm.umd.edu/shibboleth-idp/profile/cas/login?service='
         self.umd_cas_logout_url = 'https://shib.idm.umd.edu/shibboleth-idp/profile/cas/logout'
         self.umd_cas_validate_url = 'https://shib.idm.umd.edu/shibboleth-idp/profile/cas/serviceValidate?service='
-        self.umd_cas_login_url = self.umd_cas_ticket_url + urllib.parse.quote_plus(self.host_name) + urllib.parse.quote_plus(self.post_auth_redirect_route)
+        self.umd_cas_login_url = self.umd_cas_ticket_url + urllib.parse.quote_plus(self.host_name + self.post_auth_redirect_route)
 
     def get_login_cas_url(self):   
         return self.umd_cas_login_url
@@ -28,17 +28,16 @@ class UMDCASClient(object):
 
     
     def validate_ticket(self, req):
-
         ticket = getattr(req, 'args', getattr(req, 'GET', {'ticket': req})).get('ticket')
 
         if ticket is None:
             raise ValueError("ticket is None can't validate user")
         
-        xmlDict = {}
-        casValidateUrl = self.umd_cas_validate_url + urllib.parse.quote_plus(self.host_name + self.post_auth_redirect_route)  + '&ticket=' + str(ticket)
-        xmlDump = urlopen(casValidateUrl).read().strip().decode('utf8', 'ignore')
-        xmlDict = parse(xmlDump)
+        xml_dict = {}
+        cas_validate_url = self.umd_cas_validate_url + urllib.parse.quote_plus(self.host_name + self.post_auth_redirect_route)  + '&ticket=' + str(ticket)
+        xml_dict = parse(urlopen(cas_validate_url).read().strip().decode('utf8', 'ignore'))
+
         try:
-            return xmlDict["cas:serviceResponse"]["cas:authenticationSuccess"]["cas:user"]
+            return xml_dict["cas:serviceResponse"]["cas:authenticationSuccess"]["cas:user"]
         except Exception as err:
             raise Exception("incorrect or empty xml: " + str(err))
