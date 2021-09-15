257d7354dde239cd6f4727fefb8b83403081d187
cobywinfield <coby.winfield@gmail.com>
2021-09-14 20:32:10 -0400

    Pip package

---
+++ umd_python_cas/__init__.py
@@ -0,0 +1,10 @@
+"""
+pip-test
+A test pip package.
+"""
+
+__version__ = "1.0.0"
+__author__ = 'Jacob (Coby) Winfield & Johan Vandegriff'
+__credits__ = 'Daniel Laurence & Jim Purtilo & Ronel Aguilar'
+
+from umd_python_cas.umd_python_cas import UMDCASClient
