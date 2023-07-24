964f925695448f45426cd0f6c375c827538582d1
Coby Winfield <coby.winfield@gmail.com>
2023-07-24 19:24:36 -0400

    Update umd_python_cas.py
    Simplified class logic

--- umd_python_cas/umd_python_cas.py
+++ umd_python_cas/umd_python_cas.py
@@ -15,27 +15,24 @@ class UMDCASClient(object):
 
         self.host_name = host_name
         self.post_auth_redirect_route = post_auth_route
-        self.umd_cas_ticket_url = 'https://shib.idm.umd.edu/shibboleth-idp/profile/cas/login?service='
-        self.umd_cas_logout_url = 'https://shib.idm.umd.edu/shibboleth-idp/profile/cas/logout'
-        self.umd_cas_validate_url = 'https://shib.idm.umd.edu/shibboleth-idp/profile/cas/serviceValidate?service='
-        self.umd_cas_login_url = self.umd_cas_ticket_url + urllib.parse.quote_plus(self.host_name + self.post_auth_redirect_route)
 
     def get_login_cas_url(self):   
-        return self.umd_cas_login_url
+        return 'https://shib.idm.umd.edu/shibboleth-idp/profile/cas/login?service=' + urllib.parse.quote_plus(self.host_name + self.post_auth_redirect_route)
 
     def get_logout_cas_url(self):
-        return self.umd_cas_logout_url
+        return 'https://shib.idm.umd.edu/shibboleth-idp/profile/cas/logout'
     
     def validate_ticket(self, req):
-        ticket = getattr(req, 'args', getattr(req, 'GET', {'ticket': req})).get('ticket')
-
-        if ticket is None:
-            raise ValueError("ticket is None can't validate user")
+        xml_dict = {}
 
         try: 
-            xml_dict = {}
-            cas_validate_url = self.umd_cas_validate_url + urllib.parse.quote_plus(self.host_name + self.post_auth_redirect_route) + '&ticket=' + str(ticket)
+            if (ticket := getattr(req, 'args', getattr(req, 'GET', {'ticket': req})).get('ticket')) is None:
+                raise ValueError("ticket is None can't validate user")
+            
+            cas_validate_url = 'https://shib.idm.umd.edu/shibboleth-idp/profile/cas/serviceValidate?service=' \
+                + urllib.parse.quote_plus(self.host_name + self.post_auth_redirect_route) + '&ticket=' + str(ticket)
             xml_dict = parse(urlopen(cas_validate_url).read().strip().decode('utf8', 'ignore'))
             return xml_dict["cas:serviceResponse"]["cas:authenticationSuccess"]["cas:user"]
+            
         except Exception as err:
             raise Exception("incorrect or empty xml: " + str(err))
